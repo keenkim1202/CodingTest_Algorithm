d636056179187dbfd9d6cb977e5588f14add87a3
KEEN <homurahomu@naver.com>
2022-02-21 17:33:32 +0900

    [UPDATE] TowSum

--- Array/TwoSum.swift
+++ Array/TwoSum.swift
@@ -9,6 +9,7 @@
  결과의 정렬은 상관없다.
  */
 
+// sol1
 class Solution {
   func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     var index: (Int, Int) = (0, 0)
@@ -16,6 +17,24 @@ class Solution {
     for i in 0..<nums.count {
       for j in 0..<nums.count {
         if (nums[i] + nums[j] == target) && (i != j) {
+          index.0 = i
+          index.1 = j
+          break
+        }
+      }
+    }
+    return [index.0, index.1]
+  }
+}
+
+// sol2
+class Solution {
+  func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var index: (Int, Int) = (0, 0)
+    
+    for i in 0..<nums.count {
+      for j in i+1..<nums.count {
+        if (nums[i] + nums[j] == target) {
           index.0 = i
           index.1 = j
           break
