4fd1774b0bbe17f36f174d729709489f87b111bf
KEEN <homurahomu@naver.com>
2022-02-23 16:47:04 +0900

    [ADD] '카펫' 풀이

---
+++ BruteForce/카펫.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+/*
+ - 문제: 카펫
+ - 링크: https://programmers.co.kr/learn/courses/30/lessons/42842
+ - key: 곱해서 n이 되는 수를 찾아보자
+ */
+
+func makeDivisorArray(_ n: Int) -> [[Int]] {
+  var num: Int = 3
+  var array: [[Int]] = []
+  
+  while num < n/2 {
+    if n % num == 0 {
+      let length = n / num
+      
+      if num >= length {
+        array.append([num, length])
+      }
+    }
+    
+    num += 1
+  }
+
+  return array
+}
+
+func solution(_ brown: Int, _ yellow: Int) -> [Int] {
+  let total = brown + yellow
+  let divisors = makeDivisorArray(total)
+  
+  for elem in divisors {
+    if (elem[0] * 2) + (elem[1] - 2) * 2 == brown {
+      return elem
+    }
+  }
+
+  return [0]
+}
